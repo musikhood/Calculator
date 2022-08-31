d20d97c17dc8437176a3fc3f0dbb00a83ad8adb1
Rafal <b.bialowski@gmail.com>
2022-08-31 19:37:17 +0200

    Create basic structure

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -17,7 +17,7 @@ function Calculator({ setTheme, theme }: Theme) {
   return (
     <div className="Calculator">
       <div className="Calculator__HeaderContainer">
-        <h2 className="Calculator__title">Calc</h2>
+        <h2 className="Calculator__title">calc</h2>
         <div className="Calculator__ThemeSwitcherContainer">
           <h4 className="Calculator__ThemeSwitcherName">THEME</h4>
           <div
@@ -30,6 +30,33 @@ function Calculator({ setTheme, theme }: Theme) {
               <p>3</p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="Calculator__body">
+        <div className="Calculator__screen">
+          <h1>123</h1>
+        </div>
+        <div className="Calculator__buttonsContainer">
+          <div className="Calculator__button">7</div>
+          <div className="Calculator__button">8</div>
+          <div className="Calculator__button">9</div>
+          <div className="Calculator__button">DEL</div>
+          <div className="Calculator__button">4</div>
+          <div className="Calculator__button">5</div>
+          <div className="Calculator__button">6</div>
+          <div className="Calculator__button">+</div>
+          <div className="Calculator__button">1</div>
+          <div className="Calculator__button">2</div>
+          <div className="Calculator__button">3</div>
+          <div className="Calculator__button">-</div>
+          <div className="Calculator__button">.</div>
+          <div className="Calculator__button">0</div>
+          <div className="Calculator__button">/</div>
+          <div className="Calculator__button">x</div>
+          <div className="Calculator__button Calculator__button--span">
+            RESET
+          </div>
+          <div className="Calculator__button Calculator__button--span">=</div>
         </div>
       </div>
     </div>
